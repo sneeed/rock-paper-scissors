3baee012e4d98b0e5ec607d9f9556225fb932c48
Jonas Franz <jonas.franz@mailbox.org>
2022-08-22 10:06:53 +0200

    Adds playRound

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-getComputerChoice();
-
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
 
   return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitalizeFirstLetter(playerSelection);
+  if (playerSelection === computerSelection) {
+    return `It's a tie! ${playerSelection} is the same as ${computerSelection}`;
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Loose! ${playerSelection} looses against ${computerSelection}`;
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
