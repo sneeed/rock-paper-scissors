ad5f7eb25f16b009b2784b34480db1160485bee6
Jonas Franz <jonas.franz@mailbox.org>
2022-08-22 10:37:15 +0200

    Optimize code

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
+let cntPlayerWon = 0;
+let cntComputerWon = 0;
+game();
+console.log("%c" + printGameWinner(), "font-weight:bold;");
 
-  return choices[Math.floor(Math.random() * choices.length)];
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Choose Rock, Paper or Scissors: ");
+    // const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -13,8 +21,10 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
+    ++cntPlayerWon;
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   } else {
+    ++cntComputerWon;
     return `You Loose! ${playerSelection} looses against ${computerSelection}`;
   }
 }
@@ -23,12 +33,19 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = "rock";
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-  }
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-game();
+function printGameWinner() {
+  let string = "Game Over. ";
+  if (cntPlayerWon === cntComputerWon) {
+    string += `It's a Tie. You and the computer both won ${cntPlayerWon} times.`;
+  } else {
+    string += `You ${
+      cntPlayerWon > cntComputerWon ? "Won" : "Lost"
+    }. You won ${cntPlayerWon} times, the computer won ${cntComputerWon} times.`;
+  }
+  return string;
+}
